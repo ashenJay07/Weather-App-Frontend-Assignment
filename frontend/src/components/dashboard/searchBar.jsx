c422606d4960c329d03ec4a0a142cbaab3f1d056
Ashen Jayarathne <ashenj.official@gmail.com>
2023-05-11 21:29:54 +0530

    Modified all the .jsx files.
    Added & removes some bootstraps from .jsx files.

--- frontend/src/components/dashboard/searchBar.jsx
+++ frontend/src/components/dashboard/searchBar.jsx
@@ -1,18 +1,20 @@
 const SearchBar = () => {
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div className="col-sm-4 search-bar">
-        <form className="d-flex" role="search">
-          <input
-            className="form-control"
-            type="search"
-            placeholder="Enter a city"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Add City
-          </button>
-        </form>
+    <div className="container-fluid">
+      <div className="d-flex justify-content-center">
+        <div className="col-sm-4 search-bar">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Enter a city"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Add City
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
