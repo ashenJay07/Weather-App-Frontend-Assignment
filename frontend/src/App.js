0f9c342b0b455b4305bb8f1f69ae84704fcfd59d
Ashen Jayarathne <ashenj.official@gmail.com>
2023-05-11 21:20:10 +0530

    Application footer implemented.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/layout/footer";
 import Header from "./components/layout/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="container-fluid footer">
+      <footer className="py-3 my-4">
+        <p className="text-center mt-2">2023 Fidenz Technologies</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
